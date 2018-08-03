08c7d5ed43549a24d0415087d9133de5f32bf6b7
Valdecir Junior <valdecir.silva9@fatec.sp.gov.br>
2018-08-03 17:51:57 -0300

    update

--- public/index.php
+++ public/index.php
@@ -1,9 +1,75 @@
 <?php
 
-require ('../bootstrap/app.php');
+define('__APP_ROOT__', dirname(__DIR__));
+require __APP_ROOT__ . '/bootstrap/app.php';
 
-require ('../routes/Routes.php');
+use routes\Router;
 
-$route = $router->getView($_GET['pg']);
+$router = new Router();
 
-require_once ($route);
+$router
+    ->get('/', function () {
+        ob_start();
+        require dirname(__DIR__) . "/resources/view/index.php";
+        $html = ob_get_contents();
+        ob_end_clean();
+        return $html;
+    })
+    ->get('/home', function () {
+        ob_start();
+        require dirname(__DIR__) . "/resources/view/index.php";
+        $html = ob_get_contents();
+        ob_end_clean();
+        return $html;
+    })
+    ->get('/configuracoes', function () {
+        ob_start();
+        require dirname(__DIR__) . "/resources/view/configs.php";
+        $html = ob_get_contents();
+        ob_end_clean();
+        return $html;
+    })
+    ->get('/search', function () {
+        ob_start();
+        require dirname(__DIR__) . "/resources/view/search.php";
+        $html = ob_get_contents();
+        ob_end_clean();
+        return $html;
+    })
+    ->get('/erro', function () {
+        ob_start();
+        require dirname(__DIR__) . "/resources/view/error.php";
+        $html = ob_get_contents();
+        ob_end_clean();
+        return $html;
+    })
+    ->get('/identifique-se', function () {
+        ob_start();
+        require dirname(__DIR__) . "/resources/view/login.php";
+        $html = ob_get_contents();
+        ob_end_clean();
+        return $html;
+    })
+    ->get('/cadastre-se', function () {
+        ob_start();
+        require dirname(__DIR__) . "/resources/view/register.php";
+        $html = ob_get_contents();
+        ob_end_clean();
+        return $html;
+    })
+    ->get('/confirm', function () {
+        ob_start();
+        require dirname(__DIR__) . "/resources/view/confirma_email.php";
+        $html = ob_get_contents();
+        ob_end_clean();
+        return $html;
+    })
+    ->get('/(\w+)', function () {
+        ob_start();
+        require dirname(__DIR__) . "/resources/view/profile.php";
+        $html = ob_get_contents();
+        ob_end_clean();
+        return $html;
+    });
+
+echo $router($router->method(), $router->uri());
